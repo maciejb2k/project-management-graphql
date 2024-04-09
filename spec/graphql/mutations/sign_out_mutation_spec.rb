304a75dfaee6a74fedcf137dffc003fecb3071bb
Maciej Biel <maciek.b2k@gmail.com>
2024-04-09 22:19:42 +0200

    Add refresh mutation specs

--- spec/graphql/mutations/sign_out_mutation_spec.rb
+++ spec/graphql/mutations/sign_out_mutation_spec.rb
@@ -24,10 +24,22 @@ module Mutations
           end
         end
 
-        context "when access token encoding is invalid" do
+        context "when access token base64 encoding is invalid" do
           it "throws an exception" do
             headers = {
-              "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+              "Authorization": "Bearer eyJhbG.eyJzdWIiOiIxwIiwibmFtZSI.SflKxwRJSMeKKk6yJV_adQssw5c"
+            }
+
+            expect do
+              post "/api/graphql", headers:, params: { query: sign_out_query }
+            end.to raise_error(JWTSessions::Errors::Unauthorized)
+          end
+        end
+
+        context "when bad access token is provided" do
+          it "throws an exception" do
+            headers = {
+              "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidXNlcl9pZCI6MSwiaWF0IjoxNTE2MjM5MDIyfQ.2QaRdUNiOh4gCqyXFv4k6_pPEtdmBeUTO9FqexiXjKM"
             }
 
             expect do
@@ -42,9 +54,7 @@ module Mutations
         let!(:tokens) { sign_in(user) }
 
         it "signs out the user" do
-          post "/api/graphql", headers: auth_headers(tokens), params: { query: sign_out_query }
-          json = JSON.parse(response.body)
-          data = json["data"]["signOut"]
+          data = sign_out(auth_headers(tokens))
 
           expect(response).to have_http_status(:ok)
           expect(data).to include("success" => true)
--- spec/support/helpers/authentication.rb
+++ spec/support/helpers/authentication.rb
@@ -2,22 +2,40 @@
 
 module Helpers
   module Authentication
-    def sign_in(user)
+    def sign_in_raw(user)
       post "/api/graphql", params: { query: sign_in_query(email: user.email, password: user.password) }
-      json = JSON.parse(response.body)
+      JSON.parse(response.body)
+    end
+
+    def sign_in(user)
+      json = sign_in_raw(user)
       json["data"]["signIn"]["token"].symbolize_keys
     end
 
-    def sign_out(headers)
+    def sign_out_raw(headers)
       post "/api/graphql", headers:, params: { query: sign_out_query }
-      json = JSON.parse(response.body)
+      JSON.parse(response.body)
+    end
+
+    def sign_out(headers)
+      json = sign_out_raw(headers)
       json["data"]["signOut"]
     end
 
-    def auth_headers(token)
+    def refresh_jwt_raw(headers)
+      post "/api/graphql", headers:, params: { query: refresh_query }
+      JSON.parse(response.body)
+    end
+
+    def refresh_jwt(headers)
+      json = refresh_jwt_raw(headers)
+      json["data"]["refresh"]["token"].symbolize_keys
+    end
+
+    def auth_headers(tokens)
       {
-        "Authorization": "Bearer #{token[:access]}",
-        "X-Refresh-Token": token[:refresh]
+        "Authorization": "Bearer #{tokens[:access]}",
+        "X-Refresh-Token": tokens[:refresh]
       }
     end
 
@@ -41,6 +59,19 @@ module Helpers
         mutation {
           signOut(input: {}) {
             success
+          }
+        }
+      GQL
+    end
+
+    def refresh_query
+      <<~GQL
+        mutation {
+          refresh(input: {}) {
+            token {
+              access
+              accessExpiresAt
+            }
           }
         }
       GQL
